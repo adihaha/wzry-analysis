c377962ea265e726334d938625592287e85bb016
adihaha <708931770@qq.com>
2026-07-27 11:59:45 +0800

    deploy server.cjs via API

---
+++ server.cjs
@@ -0,0 +1,35 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const ROOT = "/workspace";
+const PORT = 8000;
+const TYPES = {
+  ".html": "text/html; charset=utf-8",
+  ".js": "text/javascript; charset=utf-8",
+  ".css": "text/css; charset=utf-8",
+  ".json": "application/json; charset=utf-8",
+  ".png": "image/png",
+  ".svg": "image/svg+xml",
+  ".ico": "image/x-icon"
+};
+
+http.createServer((req, res) => {
+  let p = decodeURIComponent((req.url || "/").split("?")[0]);
+  if (p === "/") p = "/index.html";
+  const file = path.join(ROOT, p);
+  if (!file.startsWith(ROOT)) {
+    res.writeHead(403);
+    return res.end("forbidden");
+  }
+  fs.readFile(file, (err, data) => {
+    if (err) {
+      res.writeHead(404, { "Content-Type": "text/plain; charset=utf-8" });
+      return res.end("404 Not Found");
+    }
+    res.writeHead(200, { "Content-Type": TYPES[path.extname(file)] || "application/octet-stream" });
+    res.end(data);
+  });
+}).listen(PORT, "0.0.0.0", () => {
+  console.log("static server listening on http://0.0.0.0:" + PORT);
+});
